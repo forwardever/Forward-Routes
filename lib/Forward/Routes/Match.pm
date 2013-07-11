05591c70482e78417a83c67782c334813f177d58
forwardever <git.forwardever@googlemail.com>
2013-07-11 13:20:30 +0200

    cleanup

--- lib/Forward/Routes/Match.pm
+++ lib/Forward/Routes/Match.pm
@@ -7,10 +7,84 @@ sub new {
     return bless {}, shift;
 }
 
-sub _add_params {
+
+sub action {
+    return shift->{params}->{action};
+}
+
+
+sub app_namespace {
     my $self = shift;
-    my ($params) = @_;
-    %{$self->params} = (%$params, %{$self->params});
+    return $self->{app_namespace};
+}
+
+
+sub captures {
+    my $self = shift;
+    my ($key) = @_;
+    $self->{captures} ||= {};
+    return $self->{captures} unless defined $key && length $key;
+    return $self->{captures}->{$key};
+}
+
+
+sub class {
+    my $self = shift;
+
+    return undef unless $self->{params}->{controller};
+
+    my @class;
+
+    push @class, $self->{app_namespace} if $self->{app_namespace};
+
+    push @class, $self->{namespace} if $self->{namespace};
+
+    push @class, $self->{params}->{controller};
+
+    return join('::', @class);
+}
+
+
+sub controller {
+    my $self = shift;
+    return $self->{params}->{controller};
+}
+
+
+sub is_bridge {
+    my $self = shift;
+    my (@is_bridge) = @_;
+    return $self->{is_bridge} unless @is_bridge;
+    $self->{is_bridge} = $is_bridge[0];
+    return $self;
+}
+
+
+sub name {
+    my $self = shift;
+    return $self->{name};
+}
+
+
+sub namespace {
+    my $self = shift;
+    return $self->{namespace};
+}
+
+
+sub params {
+    my $self = shift;
+    my ($key) = @_;
+    $self->{params} ||= {};
+    return $self->{params} unless defined $key && length $key;
+    return $self->{params}->{$key};
+}
+
+
+sub _add_app_namespace {
+    my $self = shift;
+    my (@params) = @_;
+    $self->{app_namespace} = $params[0] if @params;
     return $self;
 }
 
@@ -39,37 +113,11 @@ sub _add_namespace {
 }
 
 
-sub _add_app_namespace {
-    my $self = shift;
-    my (@params) = @_;
-    $self->{app_namespace} = $params[0] if @params;
-    return $self;
-}
-
-
-sub params {
-    my $self = shift;
-    my ($key) = @_;
-    $self->{params} ||= {};
-    return $self->{params} unless defined $key && length $key;
-    return $self->{params}->{$key};
-}
-
-
-sub _set_params {
+sub _add_params {
     my $self = shift;
     my ($params) = @_;
-    $self->{params} = $params;
+    %{$self->params} = (%$params, %{$self->params});
     return $self;
-}
-
-
-sub captures {
-    my $self = shift;
-    my ($key) = @_;
-    $self->{captures} ||= {};
-    return $self->{captures} unless defined $key && length $key;
-    return $self->{captures}->{$key};
 }
 
 
@@ -81,59 +129,11 @@ sub _set_captures {
 }
 
 
-sub is_bridge {
+sub _set_params {
     my $self = shift;
-    my (@is_bridge) = @_;
-    return $self->{is_bridge} unless @is_bridge;
-    $self->{is_bridge} = $is_bridge[0];
+    my ($params) = @_;
+    $self->{params} = $params;
     return $self;
 }
-
-
-sub name {
-    my $self = shift;
-    return $self->{name};
-}
-
-
-sub controller {
-    my $self = shift;
-    return $self->{params}->{controller};
-}
-
-
-sub namespace {
-    my $self = shift;
-    return $self->{namespace};
-}
-
-
-sub app_namespace {
-    my $self = shift;
-    return $self->{app_namespace};
-}
-
-
-sub class {
-    my $self = shift;
-
-    return undef unless $self->{params}->{controller};
-
-    my @class;
-
-    push @class, $self->{app_namespace} if $self->{app_namespace};
-
-    push @class, $self->{namespace} if $self->{namespace};
-
-    push @class, $self->{params}->{controller};
-
-    return join('::', @class);
-}
-
-
-sub action {
-    return shift->{params}->{action};
-}
-
 
 1;
